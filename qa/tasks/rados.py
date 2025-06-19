2d9a26f4c3d62c21d9d5c3921b8c19597ede178d
Chris Harris <harriscr@uk.ibm.com>
2025-06-19 09:27:30 +0100

    quick test of theory

--- qa/tasks/rados.py
+++ qa/tasks/rados.py
@@ -31,7 +31,7 @@ class CephTestRados(Thrasher, Greenlet):
         log.info("CHDEBUG: Stopping the test")
         for test_id, daemon in self.daemons.items():
             log.info("CHDEBUG: Stopping instance %s", test_id)
-            daemon.signal(signal.SIGTERM)
+            daemon.stdin.close()
             # daemon.stdin.close()
 
     def join(self):
