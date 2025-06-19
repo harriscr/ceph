67b21e6f3d62480544bb2637b2bcd5a0580087c9
Chris Harris <harriscr@uk.ibm.com>
2025-06-19 09:27:30 +0100

    quick test of theory

--- qa/tasks/rados.py
+++ qa/tasks/rados.py
@@ -341,6 +341,7 @@ def task(ctx, config):
                 log.info("LEE: %s", e.args)
                 # LEE proof of concept experiment
                 log.info("LEE: timed out - closing stding")
+                self.stop()
 
             wait_for_all_active_clean_pgs = config.get("wait_for_all_active_clean_pgs", False)
             # usually set when we do min_size testing.
